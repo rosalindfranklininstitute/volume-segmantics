6d39a93ab5a0d14c3e6e5e0a8ff8217ea719392b
Luis Perdigao <perdigao1@yahoo.com>
2023-05-17 09:51:36 +0000

    _predict_single_axis_all_probs

--- volume_segmantics/model/operations/vol_seg_2d_predictor.py
+++ volume_segmantics/model/operations/vol_seg_2d_predictor.py
@@ -32,6 +32,19 @@ class VolSeg2dPredictor:
         self.model = trainer.model
 
     def _predict_single_axis(self, data_vol, output_probs=True, axis=Axis.Z):
+        '''
+        Make 2D predictions using the current model along the axis specified
+
+        Parameters:
+            data_vol: data volume
+            output_probs: True if want to return probabilities
+            axis: Can be Axis.Z, Axis.Y or Axis.X
+
+        Returns:
+            volume data in in a tuple
+            (labels, probs)
+            If output_probs was set to False, then probs will return None
+        '''
         output_vol_list = []
         output_prob_list = []
         data_vol = utils.rotate_array_to_axis(data_vol, axis)
@@ -50,6 +63,7 @@ class VolSeg2dPredictor:
                 labels = torch.argmax(probs, dim=1)  # flatten channels
                 labels = utils.crop_tensor_to_array(labels, yx_dims)
                 output_vol_list.append(labels.astype(np.uint8))
+                # Collects only the probability of the label that gives maximum probability!!
                 if output_probs:
                     # Get indices of max probs
                     max_prob_idx = torch.argmax(probs, dim=1, keepdim=True)
@@ -66,6 +80,73 @@ class VolSeg2dPredictor:
         if probs is not None:
             probs = utils.rotate_array_to_axis(probs, axis)
         return labels, probs
+
+
+    def _predict_single_axis_all_probs(self, data_vol, axis=Axis.Z):
+        '''
+        Make 2D predictions using the current model along the axis specified
+
+        Parameters:
+            data_vol: data volume
+            axis: Can be Axis.Z, Axis.Y or Axis.X
+
+        Returns:
+            volume data in in a tuple
+            (labels, probs)
+            If output_probs was set to False, then probs will return None
+        '''
+        output_vol_list = []
+        output_prob_list = []
+        data_vol = utils.rotate_array_to_axis(data_vol, axis)
+        yx_dims = list(data_vol.shape[1:])
+        s_max = nn.Softmax(dim=1)
+        data_loader = get_2d_prediction_dataloader(data_vol, self.settings)
+        self.model.eval()
+        logging.info(f"Predicting segmentation for volume of shape {data_vol.shape}.")
+        with torch.no_grad():
+            for batch in tqdm(
+                data_loader, desc="Prediction batch", bar_format=cfg.TQDM_BAR_FORMAT
+            ):
+                output = self.model(batch.to(self.model_device_num))  # Forward pass
+                probs = s_max(output)  # Convert the logits to probs
+
+                labels = torch.argmax(probs, dim=1)  # flatten channels
+                labels = utils.crop_tensor_to_array(labels, yx_dims)
+                output_vol_list.append(labels.astype(np.uint8))
+                # Collects only the probability of the label that gives maximum probability!!
+                
+                #By default, collect all the probabilities
+                # # Get indices of max probs
+                # max_prob_idx = torch.argmax(probs, dim=1, keepdim=True)
+                # # Extract along axis from outputs
+                # probs = torch.gather(probs, 1, max_prob_idx)
+                # # Remove the label dimension
+                # probs = torch.squeeze(probs, dim=1)
+                #print(f"1. probs.shape:{probs.shape}")
+                probs = utils.crop_tensor_to_array(probs, yx_dims)
+                #print(f"2. probs.shape:{probs.shape}")
+                output_prob_list.append(probs.astype(np.float16))
+
+        labels = np.concatenate(output_vol_list)
+        labels = utils.rotate_array_to_axis(labels, axis)
+        probs = np.concatenate(output_prob_list) if output_prob_list else None
+        print(f"3. probs.shape:{probs.shape}")
+
+        # Don't use rotate_array_to_axis, because probs has one extra dimension for class label
+        if probs is not None:
+            probs=np.transpose(probs,(0,2,3,1))
+            #probs= probs.swapaxes(0,1)
+            if axis == Axis.Z:
+                pass
+            if axis == Axis.Y:
+                probs=probs.swapaxes(0, 1)
+            if axis == Axis.X:
+                probs=probs.swapaxes(0, 2)
+            
+        print(f"4. probs.shape:{probs.shape}")
+
+        return labels, probs
+    
 
     # def _predict_3_ways_max_probs(self, data_vol):
     #     logging.info("_predict_3_ways_max_probs")
